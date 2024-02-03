e3ec6575b387a37fe816ff1d09824dc6b4024eb8
malzeq <145157948+malzeq@users.noreply.github.com>
2024-02-03 02:08:15 -0600

    current changes

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import discord
+import openai
 from discord.ext import commands
 from openai import OpenAI
+from .gitignore import BOT_TOKEN
+from .gitignore import CHANNEL_ID
+from .gitignore import OPENAI_API_KEY
+
 
 client = OpenAI(
   organization='moodBotBrain',
 )
+
+bot = commands.Bot(command_prefix = "mb!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Hello World!")
+
+bot.run(BOT_TOKEN)
