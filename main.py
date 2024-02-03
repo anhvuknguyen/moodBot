96f77888c164df4c5bc0304b0a9bb77d47beb7b0
anhvuknguyen <avnguyen1115@gmail.com>
2024-02-03 02:45:15 -0600

    we gettin gooooooood

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import discord
 import openai
 from discord.ext import commands
 from openai import OpenAI
-from .gitignore import BOT_TOKEN
-from .gitignore import CHANNEL_ID
-from .gitignore import OPENAI_API_KEY
 
+#MAKE THIS HACKK PROOF
+OPENAI_API_KEY = "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"
+BOT_TOKEN = "MTIwMzIzMTc1ODAxNjA1NzM2NQ.G0QNjl.UsCsq_XFQwPzcIP3AT3ACqvVQuchIS7PbnZgMY"
+CHANNEL_ID = 1203237203627737142
 
-client = OpenAI(
-  organization='moodBotBrain',
-)
+#client = OpenAI(
+#  organization='moodBotBrain',
+#)
 
 bot = commands.Bot(command_prefix = "mb!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Hello World!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("its because im moodbot")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+async def mood(ctx):
+    await ctx.send("You are sad")
 
 bot.run(BOT_TOKEN)
